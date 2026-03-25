90a8d9d223bdc3627c098206cbde0e5aa5a955ee
Gaurav Kumar Borad <gjain7764@gmail.com>
2026-03-25 13:49:04 +0530

    this is my inital commit

--- src/main/java/com/talenttalk/controller/CompanyHomeController.java
+++ src/main/java/com/talenttalk/controller/CompanyHomeController.java
@@ -1,4 +1,45 @@
 package com.talenttalk.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class CompanyHomeController {
+    @GetMapping("/companyDashboard")
+    public String CompanyDashboard(){
+        return "companyDashboard";
+    }
+    @GetMapping("/companyClient")
+    public String CompanyClient(){
+        return "companyClient";
+    }
+    @GetMapping("/companyJobs")
+    public String CompanyJobs(){
+        return "companyJobs";
+    }
+    @GetMapping("/companyPayment")
+    public String CompanyPayment(){
+        return "companyPayment";
+    }
+    @GetMapping("/companySettings")
+    public String CompanySettings(){
+        return "companySettings";
+    }
+    @GetMapping("/companyManageJobs")
+    public String CompanyManageJobs(){
+        return "companyManageJobs";
+    }
+    @GetMapping("/companyForgotPassword")
+    public String CompanyForgotPassword(){
+        return "companyForgotPassword";
+    }
+
+    @GetMapping("/companyApplication")
+    public String CompanyApplication(){
+        return "companyApplication";
+    }
+    @GetMapping("/companyMessage")
+    public String CompanyMessage(){
+        return "companyMessage";
+    }
 }
